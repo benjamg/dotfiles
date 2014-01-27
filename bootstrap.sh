969a2a25f7e400f72e3cfa614118afa7fa0f3ecd
Ben Gray <ben@benjamg.com>
2014-01-27 19:39:26 +0000

    git without clone doesn't actually work

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # TODO: check I'm still using ubuntu, as this is unlikely to work elsewhere
 
 sudo apt-get -y install git
-git git@github.com:benjamg/dotfiles .dotfiles
+git clone git@github.com:benjamg/dotfiles .dotfiles
 
 basedir=`pwd`
 
