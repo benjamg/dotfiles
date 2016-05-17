251db6f6610b3d54a92232987f3d8825ceaa3803
Ben Gray <ben.gray@datasift.com>
2016-05-17 08:49:23 +0100

    tidy up tmp

--- modules/libzmqpp/setup.sh
+++ modules/libzmqpp/setup.sh
@@ -1,9 +1,6 @@
 # Build and install things needed for dev system that isn't in apt (or we use a different version than in apt)
 cores=`nproc`
 
-mkdir tmp
-pushd tmp
-
 # Zmqpp - DS fork
 git clone git://github.com/datasift/zmqpp.git
 pushd zmqpp
